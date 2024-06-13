3328afa6e43b3dfbfe642fbd5e6d70de6db6a5b9
cperdomo12cr <cperdomo@arcetec.com.co>
2024-06-13 12:00:56 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/homePage/homePage.component';
 import { AbouthPageComponent } from './shared/pages/abouth-page/abouth-page.component';
+import { ContactComponent } from './shared/pages/contact/contact.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent
   },
   {
     path: 'about',
     component: AbouthPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: '**',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,20 +3,25 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/homePage/homePage.component';
 import { AbouthPageComponent } from './pages/abouth-page/abouth-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 
 @NgModule({
   declarations: [
     AbouthPageComponent,
+    ContactComponent,
     HomePageComponent,
     SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AbouthPageComponent,
+    ContactComponent,
     HomePageComponent,
     SidebarComponent,
   ]
