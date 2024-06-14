e8636eb283b2ce2d13d2391b93adab6c4771909a
cperdomo12cr <cperdomo@arcetec.com.co>
2024-06-13 19:30:50 -0500

    fiix

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, Observable, of, map } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -9,6 +9,15 @@ export class CountriesService {
   private apiUrl = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
+
+  searchCountryByAlpha(code: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url)
+    .pipe(
+      map(countries => countries.length > 0 ? countries[0] : null),
+      catchError( () => of(null) )
+    );
+  }
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
